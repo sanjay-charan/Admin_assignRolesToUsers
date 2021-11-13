ad38d766f2ae9aaaa83611f209d2cd7a96144093
RamRomOfficial <m.sriram0696@gmail.com>
2021-11-14 00:14:19 +0530

    Integrated Rolemanagement

---
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.util.Optional;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.demo.model.rolemanagement.ERole;
+import com.example.demo.model.rolemanagement.Role;
+
+public interface RoleRepository extends MongoRepository<Role, String>{
+	Optional<Role> findByName(ERole name);
+}
