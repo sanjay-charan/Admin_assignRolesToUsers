41d8a185447ff841e2ed96726b9eb41c1a7b71a1
sanjay14301 <sanjay14301@DESKTOP-3U60KQL>
2021-11-14 00:28:29 +0530

    sanjay commit

--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -3,7 +3,14 @@ package com.example.demo.exception;
 public class BadRequestException extends RuntimeException{
 
 	/**
-	 * 
+	 * Method to Route the process to corresponding handler to get the complete
+	 * response
+	 *
+	 * @param context APIContext with the request details.
+	 * @param view    contains the request body.
+	 * @return ResponseEntity with respective status and information.
+	 * @throws APIException handles Exception from Handler class.
+
 	 */
 	private static final long serialVersionUID = 1L;
 
